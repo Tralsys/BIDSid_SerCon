669280d1272bb48ac2dcfe26a284f5a2090addb5
Tetsu Otter <tetsu_otter@aol.com>
2018-09-30 22:23:11 +0900

    通信機能の仮実装

--- BIDSid_SerCon/MainWindow.xaml.cs
+++ BIDSid_SerCon/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Mackoy.Bvets;
 namespace BIDSid_SerCon
 {
   /// <summary>
@@ -19,9 +22,229 @@ namespace BIDSid_SerCon
   /// </summary>
   public partial class MainWindow : Window
   {
+    public bool IsSettingChanged = false;
     public MainWindow()
     {
       InitializeComponent();
+    }
+
+    private void CancelEv(object sender, RoutedEventArgs e)
+    {
+      Close();
+    }
+
+    private void OnLoad(object sender, RoutedEventArgs e)
+    {
+      //接続状況表示更新
+      if (ID.IsBIDSppConnected && ID.BIDSppVersion > 0)
+      {
+        BIDSppConnectEllipse.Fill = new SolidColorBrush(Colors.LightGreen);
+        BIDSppVerLab.Content = ID.BIDSppVersion.ToString();
+      }
+      else
+      {
+        BIDSppConnectEllipse.Fill = new SolidColorBrush(Colors.Red);
+        BIDSppVerLab.Content = string.Empty;
+      }
+      if (ID.IsSerialConnected) SerialConnectEllipse.Fill = new SolidColorBrush(Colors.LightGreen);
+      else SerialConnectEllipse.Fill = new SolidColorBrush(Colors.Red);
+
+      List<string> PortList = new List<string>();
+      for(int i = 0; i < 256; i++)
+      {
+        PortList.Add("COM" + i.ToString());
+      }
+      COMPortListBox.ItemsSource = PortList;
+      BaudRateListBox.ItemsSource = BaudRateList;
+      COMPortListBox.SelectedItem = Properties.Settings.Default.COMPortName;
+      BaudRateListBox.SelectedItem = Properties.Settings.Default.BaudRateNum;
+
+
+      BIDSppVerLab.Content = ID.size.ToString();
+    }
+    private readonly List<int> BaudRateList = new List<int>() { 4800, 9600, 19200, 38400, 57600, 115200 };
+    private void EnterEv(object sender, RoutedEventArgs e)
+    {
+      if (Properties.Settings.Default.COMPortName != (string)COMPortListBox.SelectedItem ||
+      Properties.Settings.Default.BaudRateNum != BaudRateList[BaudRateListBox.SelectedIndex]) 
+      {
+        Properties.Settings.Default.COMPortName = (string)COMPortListBox.SelectedItem;
+        Properties.Settings.Default.BaudRateNum = BaudRateList[BaudRateListBox.SelectedIndex];
+        Properties.Settings.Default.Save();
+        IsSettingChanged = true;
+      }
+      Close();
+    }
+
+  }
+
+  public class ID : IInputDevice
+  {
+    public event Mackoy.Bvets.InputEventHandler LeverMoved;
+    public event Mackoy.Bvets.InputEventHandler KeyDown;
+    public event Mackoy.Bvets.InputEventHandler KeyUp;
+    static public bool IsSerialConnected { get; private set; } = false;
+    static public bool IsBIDSppConnected { get; private set; } = false;
+    static public int BIDSppVersion { get; private set; } = 0;
+    static private MainWindow mw = new MainWindow();
+    public void Configure(System.Windows.Forms.IWin32Window owner)
+    {
+      mw.Show();
+    }
+    private static bool Disposing = false;
+    private static bool Disposed = false;
+    Thread SerTh = new Thread(new ThreadStart(SerialDoing));
+    public void Dispose()
+    {
+      Disposing = true;
+      UnmapViewOfFile(pMemory);
+      CloseHandle(hSharedMemory);
+      Properties.Settings.Default.Save();
+      for (int i = 0; i < 30; i++) if (!Disposed && SerTh.IsAlive) Thread.Sleep(100);
+      SerTh.Abort();
+    }
+
+    public void Load(string settingsPath)
+    {
+      Properties.Settings.Default.Upgrade();
+      SerTh.Start();
+    }
+
+
+    public void Tick()
+    {
+      BSMD = (BIDSSharedMemoryData)Marshal.PtrToStructure(pMemory, typeof(BIDSSharedMemoryData));
+    }
+
+    public void SetAxisRanges(int[][] ranges)
+    {
+
+    }
+
+    private static string SRAMName = "BIDSSharedMem";
+    //SECTION_ALL_ACCESS=983071
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr CreateFileMapping(UIntPtr hFile, IntPtr lpAttributes, uint flProtect, uint dwMaximumSizeHigh, uint dwMaximumSizeLow, string lpName);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr MapViewOfFile(IntPtr hFileMappingObject, uint dwDesiredAccess, uint dwFileOffsetHigh, uint dwFileOffsetLow, uint dwNumberOfBytesToMap);
+    //[DllImport("kernel32.dll")]
+    //static extern void CopyMemory(IntPtr Destination, IntPtr Source, uint Length);//予約
+    [DllImport("kernel32.dll")]
+    static extern bool UnmapViewOfFile(IntPtr lpBaseAddress);
+    [DllImport("kernel32.dll")]
+    static extern bool CloseHandle(IntPtr hObject);
+
+
+    public struct Spec
+    {
+      public int B;  //ブレーキ段数
+      public int P;  //ノッチ段数
+      public int A;  //ATS確認段数
+      public int J;  //常用最大段数
+      public int C;  //編成車両数
+    };
+    public struct State
+    {
+      public double X; //列車位置[m]
+      public float V;  //列車速度[km/h]
+      public int T;    //0時からの経過時間[ms]
+      public float BC; //BC圧力[kPa]
+      public float MR; //MR圧力[kPa]
+      public float ER; //ER圧力[kPa]
+      public float BP; //BP圧力[kPa]
+      public float SAP;  //SAP圧力[kPa]
+      public float I;  //電流[A]
+    };
+    public struct Hand
+    {
+      public int B;  //ブレーキハンドル位置
+      public int P;  //ノッチハンドル位置
+      public int R;  //レバーサーハンドル位置
+      public int C;  //定速制御状態
+    };
+    public struct Beacon
+    {
+      public int Num;  //Beaconの番号
+      public int Sig;  //対応する閉塞の現示番号
+      public float X;  //対応する閉塞までの距離[m]
+      public int Data; //Beaconの第三引数の値
+    };
+    //Version 200ではBeaconData,IsKeyPushed,SignalSetIntはDIsabled
+    public struct BIDSSharedMemoryData
+    {
+      public bool IsEnabled;
+      public int VersionNum;
+      public Spec SpecData;
+      public State StateData;
+      public Hand HandleData;
+      public bool IsDoorClosed;
+      [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+      public int[] Panel;
+      [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+      public int[] Sound;
+
+
+      //[MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+      //public Beacon[] BeaconData;
+      //[MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+      //public bool[] IsKeysPushed;
+      //public int SignalSetInt;
+    };
+    static public uint size = (uint)Marshal.SizeOf(typeof(BIDSSharedMemoryData));
+    static IntPtr hSharedMemory = CreateFileMapping(UIntPtr.Zero, IntPtr.Zero, 4, 0, size, SRAMName);
+    static IntPtr pMemory = MapViewOfFile(hSharedMemory, 983071, 0, 0, size);
+    BIDSSharedMemoryData BSMD = new BIDSSharedMemoryData();
+    private static byte[] GetAndWriteByte(byte[] GetArray)
+    {
+      byte[] ReturnArray = new byte[32];
+      ReturnArray[0] = GetArray[0];
+      ReturnArray[1] = GetArray[1];
+      ReturnArray[30] = GetArray[30];
+      ReturnArray[31] = GetArray[31];
+      switch (Convert.ToInt16(GetArray.Take(2).ToArray()))
+      {
+        case 0:
+          break;
+      }
+      return ReturnArray;
+    }
+
+
+    private static void SerialDoing()
+    {
+      Disposing = false;
+      while (!Disposing)
+      {
+        using (SerialPort SP = new SerialPort(Properties.Settings.Default.COMPortName, Properties.Settings.Default.BaudRateNum))
+        {
+          try
+          {
+            SP.Open();
+          }
+          catch (Exception e)
+          {
+            MessageBoxResult mbr = MessageBox.Show(e.Message+"\n再接続を試行しますか？", "BIDS SerialConverter", MessageBoxButton.YesNo);
+            if (mbr == MessageBoxResult.No)
+            {
+              Disposing = true;
+            }
+          }
+          byte[] b = new byte[32];
+          while (!mw.IsSettingChanged || !Disposing || SP.IsOpen)
+          {
+            b = new byte[32];
+            SP.Read(b, 0, 32);
+            SP.Write(GetAndWriteByte(b), 0, 32);
+            if (Convert.ToInt16(b.Take(2).ToArray()) == 11)
+            {
+              SP.Close();
+            }
+          }
+          if (SP.IsOpen) SP.Close();
+          Thread.Sleep(50);
+        }
+      }
+      Disposed = true;
     }
   }
 }
