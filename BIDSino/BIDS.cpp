83444289be7d32ad46788b965b37d0ba4637fc14
stop-pattern <railwaysandstationfan+Git@gmail.com>
2019-01-08 02:24:27 +0900

    実装部分離

---
+++ BIDSino/BIDS.cpp
@@ -0,0 +1,30 @@
+#include <string.h> 
+#include "BIDS.h"
+
+float SerialGet(String Command) {
+  Serial.print(Command);
+  Serial.print("\n");
+  //Serial.println();
+  while (Serial.available() <= 0);
+  String GetData = Serial.readStringUntil('\n');
+  GetData.replace("\n", "");
+  if (GetData.startsWith(Command)) {
+    GetData.replace(Command + "X", "");
+    return GetData.toFloat();
+  }
+  return 0;
+}
+
+float DataGet(String Identifier, String Symbol, int Num) {
+  return SerialGet("TR" + Identifier + Symbol + String(Num));
+}
+float DataGet(String Identifier, int Num) {
+  return DataGet(Identifier, "", Num);
+}
+
+int VersionCheck(int VersionNum) {
+  int ret;
+  int vnum = DataGet("V", VersionNum);
+  if (vnum < VersionNum) VersionNum = vnum;
+  return ret;
+}
--- BIDSino/BIDS.h
+++ BIDSino/BIDS.h
@@ -1,32 +1,5 @@
-int VersionCheck(int vnum) {
-  String Command = "TRV" + vnum;
-  Serial.print(Command);
-  while (Serial.available() <= 0);
-  String GetData = Serial.readStringUntil('\n');
-  GetData.replace("\n", "");
-  if (GetData.startsWith(Command)) {
-    GetData.replace(Command + "X", "");
-    return GetData.toFloat();
-  }
-}
-
-float SerialGet(String Command) {
-  Serial.print(Command);
-  Serial.print("\n");
-  //Serial.println();
-  while (Serial.available() <= 0);
-  String GetData = Serial.readStringUntil('\n');
-  GetData.replace("\n", "");
-  if (GetData.startsWith(Command)) {
-    GetData.replace(Command + "X", "");
-    return GetData.toFloat();
-  }
-  return 0;
-}
-
-float DataGet(String Identifier, String Symbol, int Num) {
-  return SerialGet("TR" + Identifier + Symbol + String(Num));
-}
-float DataGet(String Identifier, int Num) {
-  return DataGet(Identifier, "", Num);
-}
+#include <string.h> 
+float SerialGet(String);
+float DataGet(String, String, int);
+float DataGet(String, int);
+int VersionCheck(int);
