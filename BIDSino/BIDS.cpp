e6f8ce3ea71aab70dfb861a83ad0f65e6cff4ab3
stop-pattern <railwaysandstationfan+Git@gmail.com>
2019-04-05 02:38:00 +0900

    [add] add definition of constant value as enum
    add 5 enumerations for coding easily
    - Reverser forTRR
    - key for TRK
    - Car for TRIC
    - E for TRIE
    - Handle for TRIH
    however, E is tentative

--- BIDSino/BIDS.cpp
+++ BIDSino/BIDS.cpp
@@ -28,3 +28,56 @@ int VersionCheck(int VersionNum) {
   ret = vnum < VersionNum ? VersionNum : vnum;
   return ret;
 }
+
+
+enum Reverser : int {
+  Rear = -1,    //後
+  Neutral = 0,  //切
+  Front = 1,    //前
+};
+enum key : int {
+  Horn_1,               //default:enter
+  Horn_2,               //default:+
+  MusicHorn,            //default:-
+  ConstantSpeedControl, //default:BackSpace
+  S,                    //default:Space
+  A_1,                  //default:Insert
+  A_2,                  //default:Delete
+  B_1,                  //default:Home
+  B_2,                  //default:End
+  C_1,                  //default:PageUp
+  C_2,                  //default:Next
+  D,                    //default:2
+  E,                    //default:3
+  F,                    //default:4
+  G,                    //default:5
+  H,                    //default:6
+  I,                    //default:7
+  J,                    //default:8
+  K,                    //default:9
+  L,                    //default:0
+};
+enum Car : int {
+  BrakeNotchCount,  //Number of Brake Notches
+  PowerNotchCount,  //Number of Power Notches
+  ATSNotchCount,    //ATS Cancel Notch
+  B67NotchCount,    //80% Brake (67 degree)
+  CarNumber,        //Number of Cars
+};
+enum E : int {
+  Location,     //Train Position (Z-axis) [m]
+  Speed,        //Train Speed [km/h]
+  Time,         //Time [ms]
+  BcPressure,   //Pressure of Brake Cylinder [Pa]
+  MrPressure,   //Pressure of MR [Pa]
+  ErPressure,   //Pressure of ER [Pa]
+  BpPressure,   //Pressure of BP [Pa]
+  SapPressure,  //Pressure of SAP [Pa]
+  Current,      //Current [A]
+};
+enum Handle : int {
+  BrakeNotch,     //Brake Notch
+  PowerNotch,     //Power Notch
+  Reverser,       //Reverser Position
+  ConstantSpeed,  //Constant Speed Control
+};
--- BIDSino/BIDS.h
+++ BIDSino/BIDS.h
@@ -2,3 +2,9 @@ float SerialGet(String);
 float DataGet(String, String, int);
 float DataGet(String, int);
 int VersionCheck(int);
+
+enum Reverser : int;
+enum key : int;
+enum Car : int;
+enum E : int;
+enum Handle : int;
